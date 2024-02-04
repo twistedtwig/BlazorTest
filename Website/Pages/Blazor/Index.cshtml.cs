0381ab5ceca0527fffaca162bf4fc7e4dd571594
Jon Hawkins <jonathan1@houseofhawkins.com>
2024-02-04 17:05:30 +0000

    basic blazor in mvc

---
+++ Website/Pages/Blazor/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages.Blazor
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
